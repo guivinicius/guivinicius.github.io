034007a7c8220a43a0565eea0b2cf6e26adfad50
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2026-08-30 19:27:51 -0700

    Add bilingual Vancouver immigration guide articles and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/*.css", "./index.html"],
+  content: ["./css/*.css", "./**/*.html"],
   theme: {
     extend: {},
   },
